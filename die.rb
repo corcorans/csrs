aeb6817301e215793b975d83036793d0f5401d41
corcorans <corcorans13@gmail.com>
2013-11-20 16:56:58 -0600

    Simplified class.

--- die.rb
+++ die.rb
@@ -2,34 +2,32 @@
 # A die of any number of sides can be used.
 class Die
 
-   attr_reader :sides, :value, :number
+  # Custom exception class.
+  class InvalidSidesException < Exception ; end
 
   # Needs the number of die and the number of sides the die has.
-  def initialize(number, sides)
+  def initialize(sides)
     @sides = sides
-    @number = number
   end
   
   # Performs a random roll based upon the number of sides the die has.
   def roll()
-    if @sides != 0
-      if @sides >= 6 
-        @value = rand(@sides) + 1
-      else
-        raise "Please enter an appropriate number of sides for a die."
-      end
-    else
-      raise "Please enter the number of sides for a die."
-    end
+    raise InvalidSidesException.new("A number of sides is needed for a die.") if @sides == 0
+    raise InvalidSidesException.new("An appropriate number of sides is required.") if @sides < 6
+    
+    rand(@sides) + 1
   end
 end
 
 # Argument 1 (ARGV[0]) is the number of die
 # Argument 2 (ARGV[1]) is the number of sides for the die
-die = Die.new(ARGV[0].to_i, ARGV[1].to_i)
+die = Die.new(ARGV[1].to_i)
+count = ARGV[0].to_i
 
-# Print out the value rolled for each number of die
-for i in 1..die.number()
-  die.roll()
-  puts "You rolled a #{die.value} \n" 
+begin
+  count.times do |i|
+    puts "Die #{i + 1} is #{die.roll}"
+  end
+rescue Die::InvalidSidesException => e
+  puts e.message
 end
