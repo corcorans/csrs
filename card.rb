532853c4bad3c0f03b1cef872faca3637ae45e24
corcorans <corcorans13@gmail.com>
2013-11-20 09:08:33 -0600

    Cleaned up logic and removed unused code.

--- card.rb
+++ card.rb
@@ -5,7 +5,7 @@ class Card
   SUITES = %w(hearts diamonds clubs spades).freeze
   VALUES = %w(2 3 4 5 6 7 8 9 10 11 12 13 14).freeze
   
-  attr_reader  :count, :card, :used
+  attr_reader  :count, :used
 
   # Initialized the count to the parameter and the used array.
   def initialize(count)
@@ -15,29 +15,36 @@ class Card
 
   # Picks the card randomly.
   def pick()    
-    rand_value = rand(VALUES.length)
-    rand_suite = rand(SUITES.length)    
-
-    @check = VALUES[rand_value]
+    card  = determine_card()
     
-    case VALUES[rand_value].to_i
-    when 2..10
-      @card = "#{VALUES[rand_value]} of #{SUITES[rand_suite]}"
-    when 11
-      @card = "Jack of #{SUITES[rand_suite]}"
-    when 12
-      @card = "Queen of #{SUITES[rand_suite]}"
-    when 13
-      @card = "King of #{SUITES[rand_suite]}"
-    when 14
-      @card = "Ace of #{SUITES[rand_suite]}"
-    end
-    
-    if @used.include? @card
+    # If the card has already been picked, pick again.
+    if @used.include? card
       self.pick()
     end
     
-    @used << @card
+    # Add the card to the used card array.
+    @used << card
+    
+    return card
+  end
+  
+  # Determine the value and suite of the card.
+  def determine_card()
+    rand_value = rand(VALUES.length)
+    rand_suite = rand(SUITES.length)
+  
+    case VALUES[rand_value].to_i
+      when 2..10
+        return "#{VALUES[rand_value]} of #{SUITES[rand_suite]}"
+      when 11
+        return "Jack of #{SUITES[rand_suite]}"
+      when 12
+        return "Queen of #{SUITES[rand_suite]}"
+      when 13
+        return "King of #{SUITES[rand_suite]}"
+      when 14
+        return "Ace of #{SUITES[rand_suite]}"
+    end
   end
 end
 
@@ -47,9 +54,8 @@ card = Card.new(ARGV[0].to_i)
 # You must at least have one card to pick.
 if card.count > 0 && card.count < 53
   for i in 1..card.count
-    card.pick()
-    puts "Card #{i} is a #{card.card} \n"
+    puts "Card #{i} is a #{card.pick()} \n"
   end
 else
-  raise "The number of cards to be picked is invalid please pick between 1 and 52."
+  raise "The number of cards to be picked is invalid, please pick between 1 and 52."
 end
