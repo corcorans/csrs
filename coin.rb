2f55b1e6c5e646355a3cc552cc1be32cf12cae70
corcorans <corcorans13@gmail.com>
2013-11-21 09:26:56 -0600

    Simplifed even more since I had a useless variable.

--- coin.rb
+++ coin.rb
@@ -1,21 +1,16 @@
 # Class that simulates a coin flip which also allows multiple coins.
 class Coin
-  attr_reader :count
 
-  # Needs the number of coins that are to be flipped
-  def initialize(count)
-    @count = count
-  end
-  
   # Performs the flip for a coin.
-  def flip()
+  def flip
     (1 + rand(2)) == 1 ? "heads" : "tails"
   end
 end
 
 # Argument (ARGV[0]) is the number of coins to be flipped.
-coin = Coin.new(ARGV[0].to_i)
+coin = Coin.new
+count = ARGV[0].to_i
 
-coin.count.times do |i|
-  puts "Coin #{i + 1} was #{coin.flip()}\n"
+count.times do |i|
+  puts "Coin #{i + 1} was #{coin.flip}\n"
 end
