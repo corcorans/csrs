72c3de617f03b234af8b65629d7268fd1d4efcc5
corcorans <corcorans13@gmail.com>
2013-11-21 09:23:35 -0600

    Simplified class and removed for loop.

--- coin.rb
+++ coin.rb
@@ -1,27 +1,21 @@
 # Class that simulates a coin flip which also allows multiple coins.
 class Coin
-   attr_reader :coins, :flip, :value
+  attr_reader :count
 
   # Needs the number of coins that are to be flipped
-  def initialize(coins)
-    @coins = coins
+  def initialize(count)
+    @count = count
   end
   
   # Performs the flip for a coin.
   def flip()
-    if @coins > 0 
-      @flip = 1 + rand(2)
-      @value = @flip == 1 ? "heads" : "tails"
-    else
-      raise "The number of coin(s) must be at least one."
-    end
+    (1 + rand(2)) == 1 ? "heads" : "tails"
   end
 end
 
 # Argument (ARGV[0]) is the number of coins to be flipped.
 coin = Coin.new(ARGV[0].to_i)
 
-for i in 1..coin.coins()
-  coin.flip()
-  puts "Coin #{i} was #{coin.value} \n"
+coin.count.times do |i|
+  puts "Coin #{i + 1} was #{coin.flip()}\n"
 end
