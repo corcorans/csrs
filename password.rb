0ee62cb54ed27c1b410547fa45bc0be07f0acc31
corcorans <corcorans13@gmail.com>
2013-11-20 19:06:22 -0600

    Simplified class.

--- password.rb
+++ password.rb
@@ -1,30 +1,25 @@
 # Class that randomly generates a password key.
 class Password
-  
+
   # Constant of characters to be used in the password
   CHARACTERS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a + ('!'..'/').to_a
   
-  attr_reader :number
-  
   # Requires the length for the length of the password
-  def initialize(length, number)
-    @@length = length
-    @number = number
+  def initialize(length)
+    @length = length
   end
   
   # Generates the random password.
   def generate()
-    CHARACTERS.sort_by{ rand }.join[0..@@length]
+    CHARACTERS.sort_by{ rand }.join[0..@length]
   end
 end
 
 # Argument 1 (ARGV[0]) is the length of the password.
 # Argument 2 (ARGV[1]) is the number of passwords you want to generate.
-password = Password.new(ARGV[0].to_i, ARGV[1].to_i)
+password = Password.new(ARGV[0].to_i)
+count =  ARGV[1].to_i
 
-# Keep generating passwords until the given amount.
-count = 0
-until count == password.number
-  puts password.generate() + "\n"
-  count += 1
+count.times do
+   puts password.generate() + "\n"
 end
